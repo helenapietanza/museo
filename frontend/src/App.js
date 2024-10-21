bcf94af0206fa3573ddb60e9afa69fd523c8dac5
Helena <pietanzahelena@gmail.com>
2024-10-21 19:44:42 +0200

    Resa navbar sticky

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
-import "./components/slideshow.css";
+import "./components/css/nav.css";
+import "./components/css/slideshow.css";
+import "./components/css/immaginefrontale.css";
 import Navtop from "./components/nav.jsx";
 import FrontImage from "./components/frontimage.jsx";
 import Slideshow from "./components/slideshow.jsx";
