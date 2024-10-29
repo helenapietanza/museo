d80270ee4c0e60d1490db496f418bf7df00c7008
Helena <pietanzahelena@gmail.com>
2024-10-29 10:39:57 +0100

    Update file app.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from "express";
 const app = express();
+import expressLayouts from "express-ejs-layouts";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+app.set("layout", "layout");
+app.use("/frontend", express.static("public"));
 
 app.get("/home", (req, res) => {
-  res.send("hello world");
+  res.render("home");
 });
 
 const PORT = process.env.PORT || 8000;
